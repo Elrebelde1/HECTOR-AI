9c12c844d1b07c6a111e3f773e2005e1a17f960f
Wichi <sebastianbarbaro82@gmail.com>
2025-04-26 14:23:55 -0400

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,49 +1,49 @@
-import { igdl } from 'ruhend-scraper';
+import { igdl } from 'ruhend-scraper'
 
-const handler = async (m, { text, conn, args, usedPrefix, command }) => {
+const handler = async (m, { text, conn, args }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar ❤️‍🔥\`*', m, fake);
+    return conn.reply(m.chat, `🔥{emoji} Por favor, ingresa un enlace de Facebook.`, m)
   }
 
-  await m.react('🕒');
   let res;
   try {
+    await m.react(rwait);
     res = await igdl(args[0]);
-  } catch (error) {
-    return conn.reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
+  } catch (e) {
+    return conn.reply(m.chat, `${msm} Error al obtener datos. Verifica el enlace.`, m)
   }
 
   let result = res.data;
   if (!result || result.length === 0) {
-    return conn.reply(m.chat, '*`No se encontraron resultados.`*', m);
+    return conn.reply(m.chat, `🔥{emoji2} No se encontraron resultados.`, m)
   }
 
   let data;
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
-  } catch (error) {
-    return conn.reply(m.chat, '*`Error al procesar los datos.`*', m);
+  } catch (e) {
+    return conn.reply(m.chat, `${msm} Error al procesar los datos.`, m)
   }
 
   if (!data) {
-    return conn.reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
+    return conn.reply(m.chat, `${emoji2} No se encontró una resolución adecuada.`, m)
   }
 
-  await m.react('✅');
   let video = data.url;
-
   try {
-    await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
-  } catch (error) {
-    return conn.reply(m.chat, '*`Error al enviar el video.`*', m);
-  await m.react('❌');
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
+    await m.react(done);
+  } catch (e) {
+    return conn.reply(m.chat, `${msm} Error al enviar el video.`, m)
+    await m.react(error);
   }
-};
+}
 
-handler.help = ['fb *<link>*'];
-handler.estrellas = 2
-handler.tags = ['downloader']
-handler.command = /^(fb|facebook|fbdl)$/i;
-handler.register = true
+handler.help = ['facebook', 'fb']
+handler.tags = ['descargas']
+handler.command = ['facebook', 'fb']
+handler.group = true;
+handler.register = true;
+handler.coin = 2;
 
-export default handler;                                                                                                                                                                                                                                          
+export default handler
