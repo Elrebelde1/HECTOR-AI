7878799ce1a8978a6482eb851325673b4c729287
Wichi <sebastianbarbaro82@gmail.com>
2025-04-25 23:22:02 -0400

    Crear info-owner.js

---
+++ plugins/info-owner.js
@@ -0,0 +1,44 @@
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) { 
+    let numcreador = '+18303195196';
+    let ownerJid = numcreador + '@s.whatsapp.net';
+
+    let name = await conn.getName(ownerJid) || 'owner'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Bot MD ';
+
+    let empresa = 'MESITO - Servicios Tecnológicos';
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+EMAIL:
+URL:
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: name, 
+            contacts: [{ vcard }]
+        } 
+    }, { quoted: m });
+}
+
+handler.help = ['owner']; 
+handler.tags = ['main']; 
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
